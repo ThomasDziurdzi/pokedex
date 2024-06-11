b4862cef11d0271817d09174b7a7ded8507234cf
ThomasDziurdzi <thomasdziurdzi@gmail.com>
2024-06-11 12:16:12 +0200

    quete 4

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,26 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 import PokemonCard from './components/PokemonCard'
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
       <div>
-        <PokemonCard />
+        <PokemonCard pokemon={pokemonList[0]}/>
       </div>
-
     </>
   )
 }
+
+const pokemonList = [
+  {
+      name: "bulbasaur",
+      imgSrc:
+          "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
+  },
+  {
+      name: "mew",
+      
+  },
+];
 
 export default App
--- src/components/PokemonCard.jsx
+++ src/components/PokemonCard.jsx
@@ -1,13 +1,11 @@
-function PokemonCard() {
-
-    let pokemon = pokemonList[1];
-    
+function PokemonCard({pokemon}) {
+    console.log(pokemon)
 
     return (
 
         <figure>
-            
-            {pokemon.imgSrc ? (
+            {pokemon.imgSrc ? 
+            (
                 <img src={pokemon.imgSrc} alt={pokemon.name} />
             ) : (
                 <p>???</p>
@@ -17,15 +15,6 @@ function PokemonCard() {
     );
 }
 
-const pokemonList = [
-    {
-        name: "bulbasaur",
-        imgSrc:
-            "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
-    },
-    {
-        name: "mew",
-    },
-];
+
 
 export default PokemonCard;
