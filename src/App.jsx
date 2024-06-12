9bc3d9525acdc0c910b8b525b542713c11333de1
ThomasDziurdzi <thomasdziurdzi@gmail.com>
2024-06-12 23:00:12 +0200

    bouton par pokemon généré

--- src/App.jsx
+++ src/App.jsx
@@ -24,6 +24,7 @@ function App() {
         handleClickBefore = {handleClickBefore}
         handleClickNext = {handleClickNext}
         MaxIndex = {pokemonList.length - 1}  
+        PokemonList = {pokemonList}
         />
       </div>
     </>
@@ -32,26 +33,31 @@ function App() {
 
 const pokemonList = [
   {
+      id: 1,
       name: "bulbasaur",
       imgSrc:
         "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
     },
     {
+      id : 2,
       name: "charmander",
       imgSrc:
         "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/4.png",
     },
     {
+      id : 3,
       name: "squirtle",
       imgSrc:
         "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/7.png",
     },
     {
+      id : 4,
       name: "pikachu",
       imgSrc:
         "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/25.png",
     },
     {
+      id : 5,
       name: "mew",
     },
   ];
--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,19 +1,26 @@
-function NavBar (props) {
-    const {pokemonIndex, handleClickBefore, handleClickNext, MaxIndex} = props;
-    return (
-        <div>
-        {pokemonIndex > 0 ? (
-          <button onClick={handleClickBefore}>Précédent</button>
-        ) : (
-          <button style={{visibility: 'hidden'}}></button>
-        )}
-        {pokemonIndex < MaxIndex ? (
-          <button onClick={handleClickNext}>Suivant</button>
-        ) : (
-          <button style={{visibility: 'hidden'}}></button>
-        )}
-        </div>
-    )
+function NavBar(props) {
+  const { pokemonIndex, handleClickBefore, handleClickNext, MaxIndex, PokemonList } = props;
+  return (
+    <div>
+      {pokemonIndex > 0 ? (
+        <button onClick={handleClickBefore}>Précédent</button>
+      ) : (
+        <button style={{ visibility: 'hidden' }}></button>
+      )}
+      {pokemonIndex < MaxIndex ? (
+        <button onClick={handleClickNext}>Suivant</button>
+      ) : (
+        <button style={{ visibility: 'hidden' }}></button>
+      )}
+      <div>
+        {
+          PokemonList.map(pokemon => (
+            <button key={pokemon.id} onClick={() => console.log(pokemon.id)}>{pokemon.name}</button>
+          ))
+        }
+      </div>
+    </div>
+  )
 }
 
 export default NavBar
